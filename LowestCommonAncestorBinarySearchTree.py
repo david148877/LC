298d7b3c999685b18962b71ef053e77457d3d7bd
Miles <david148877@gmail.com>
2017-09-23 22:36:44 -0500

    Create LowestCommonAncestorBinarySearchTree.py

---
+++ LowestCommonAncestorBinarySearchTree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        
+        #iterative
+        while root:
+            if root.val > max(p.val, q.val):
+                root = root.left
+            elif root.val < min(p.val, q.val):
+                root = root.right
+            else:
+                return root
+        return None
